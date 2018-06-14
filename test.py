5b62dc50ec9f2771be49d1d6907db4ff45ee8ba8
ivanzvonkov <ivan.zvonkov@gmail.com>
2018-06-14 00:20:46 -0400

    evaluate works

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import time
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -11,8 +13,9 @@ def convert_header(header, index):
     return (header[index+2] * 256) + header[index+3]
 
 # returns features dict
-def load_features(filename):
+def load_features(filename, size):
     with gzip.open(filename) as bytestream:
+
         #read header
         header = bytestream.read(16)
         header = np.frombuffer(header, dtype=np.uint8)
@@ -25,12 +28,14 @@ def load_features(filename):
         data = data.reshape(num_images, IMAGE_SIZE*IMAGE_SIZE)
         data = np.array(data)
         data = data.astype(np.int32)
+        data = data[0:size]
+        data = data/255
         features = { "image": data}
 
     return features
 
 # returns label array
-def load_labels(filename):
+def load_labels(filename, size):
     with gzip.open(filename) as bytestream:
         # read header
         header = bytestream.read(8)
@@ -41,6 +46,8 @@ def load_labels(filename):
         data = bytestream.read(num_images)
         labels = np.frombuffer(data, dtype=np.uint8)
         labels = labels.astype(np.int32)
+        labels = labels[0:size]
+
     return labels
 
 
@@ -64,12 +71,12 @@ def logger(text):
 
 if __name__ == "__main__":
 
-    training_features = load_features('train-images-idx3-ubyte.gz')
-    training_targets = load_labels('train-labels-idx1-ubyte.gz')
+    training_features = load_features('train-images-idx3-ubyte.gz', 10000)
+    training_targets = load_labels('train-labels-idx1-ubyte.gz', 10000)
     logger('Training data imported')
 
-    testing_features = load_features('t10k-images-idx3-ubyte.gz')
-    testing_targets = load_labels('t10k-labels-idx1-ubyte.gz')
+    testing_features = load_features('t10k-images-idx3-ubyte.gz', 100)
+    testing_targets = load_labels('t10k-labels-idx1-ubyte.gz', 100)
     logger('Testing data imported')
 
     feature_columns = [tf.feature_column.numeric_column("image", shape=784)]
@@ -80,27 +87,66 @@ if __name__ == "__main__":
         n_classes=10,
         hidden_units=[10,10],
         optimizer=tf.train.ProximalAdagradOptimizer(
-            learning_rate=0.1,
-            l1_regularization_strength=0.001
+            learning_rate=0.01,
         )
     )
 
-    logger('Training classifier')
-    _ = dnn_classifier.train(
-        input_fn = lambda: input_function(training_features, training_targets),
-        steps=100
-    )
+    for i in range(0,10):
+        print '------------------------'
+        print 'RUN: ',i+1
+        print '------------------------'
+        start_time = time.time()
+        logger('Training classifier')
+        _ = dnn_classifier.train(
+            input_fn = lambda: input_function(training_features, training_targets),
+            steps=50,
+        )
+        end_time = time.time()
+        print 'Time: ', end_time - start_time,'\n'
 
-    logger('Predictions')
-    predictions = dnn_classifier.predict(
-        input_fn = lambda: input_function(training_features, training_targets)
-    )
-    print predictions
-    predictions = np.array([item['predictions'][0] for item in predictions])
-    mean_squared_error = metrics.mean_squared_error(predictions, training_targets)
-    root_mean_squared_error = math.sqrt(mean_squared_error)
-    print "Mean Squared Error (on training data): %0.3f" % mean_squared_error
-    print "Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error
+        start_time = time.time()
+        logger('Evaluating classifier for Training Data')
+        metrics = dnn_classifier.evaluate(
+            input_fn= lambda: input_function(training_features, training_targets),
+            steps=100
+        )
+        end_time = time.time()
+        print 'Time: ', end_time - start_time, '\n'
+
+        logger('Metrics for Training Data')
+        for m in metrics:
+            print m, metrics[m]
+        print "---"
+
+        logger('Evaluating classifier for Training Data')
+        metrics = dnn_classifier.evaluate(
+            input_fn=lambda: input_function(training_features, training_targets),
+            steps=100
+        )
+
+    logger('Metrics for Testing Data')
+    for m in metrics:
+        print m, metrics[m]
+    print "---"
+
+    # logger('Predictions for Training Data')
+    # predictions = dnn_classifier.predict(
+    #     input_fn = lambda: input_function(training_features, training_targets)
+    # )
+
+    #print predictions['probabilities']
+
+    # logger('Predictions for Testing Data')
+    # predictions = dnn_classifier.predict(
+    #     input_fn=lambda: input_function(testing_features, testing_targets)
+    # )
+    #
+    # print predictions['probabilities']
+    # predictions = np.array([item['predictions'][0] for item in predictions])
+    # mean_squared_error = metrics.mean_squared_error(predictions, training_targets)
+    # root_mean_squared_error = math.sqrt(mean_squared_error)
+    # print "Mean Squared Error (on training data): %0.3f" % mean_squared_error
+    # print "Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error
 
     #eval_result = dnn_classifier.evaluate(
     #    input_fn=lambda: input_function(testing_features, testing_targets))
